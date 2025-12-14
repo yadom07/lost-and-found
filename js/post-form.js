fcd879565c375adedef7913d9e65fd8c0f154c1a
YossakornTonkla <100203547+YossakornTonkla@users.noreply.github.com>
2025-12-14 15:01:33 +0700

    Update post-form.js

--- js/post-form.js
+++ js/post-form.js
@@ -50,7 +50,7 @@ function calculateImportanceScore(ai) {
 
 async function enrichWithAI({ title, description }) {
   try {
-    const res = await fetch("http://localhost:3000/analyze", {
+    const res = await fetch("https://lost-and-found-crba.onrender.com/analyze", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ title, description }),
@@ -371,5 +371,6 @@ document
     alert("Post created!");
     window.location.href = "index.html";
   });*/
+
 
 
